b3e2f436612cc08e208e68098d3f2231fc22463c
Suvhajit Majumder <148516507+suvhajitmajumd@users.noreply.github.com>
2026-01-19 23:07:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ document.getElementById("query").addEventListener("keypress", (e) => {
 
 /* 5. WEATHER (OpenWeatherMap) */
 // Note: Replace 'YOUR_API_KEY' with a real one from openweathermap.org
-const API_KEY = 'ecacbed1d03d3141ee4e914c06c847e8'; 
+const API_KEY = 'YOUR_API_KEY'; 
 
 navigator.geolocation.getCurrentPosition(pos => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&units=metric&appid=${API_KEY}`)
